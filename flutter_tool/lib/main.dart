45a62e8a30345cfcd47b7183dad87f678efe0188
hatlonely <hatlonely@gmail.com>
2021-08-29 00:41:54 +0800

    update

--- flutter_tool/lib/main.dart
+++ flutter_tool/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'tool'),
     );
   }
 }
